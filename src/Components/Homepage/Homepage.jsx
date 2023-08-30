0ed56ce543fd5ead11d5b5bc981232d80f07fadd
Source-Code-007 <utsho926@gmail.com>
2023-08-30 23:54:12 +0600

    first commit

---
+++ src/Components/Homepage/Homepage.jsx
@@ -0,0 +1,9 @@
+const Homepage = () => {
+    return (
+        <div>
+            This is Homepage
+        </div>
+    );
+};
+
+export default Homepage;
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,55 @@
+import { FaSearch } from "react-icons/fa";
+
+const Navbar = () => {
+    return (
+        <div className="navbar bg-green-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><a>Item 1</a></li>
+                        <li>
+                            <a>Parent</a>
+                            <ul className="p-2">
+                                <li><a>Submenu 1</a></li>
+                                <li><a>Submenu 2</a></li>
+                            </ul>
+                        </li>
+                        <li><a>Item 3</a></li>
+                    </ul>
+                </div>
+                <a className="font-semibold text-xl text-red-500">AirBnb</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="rounded-3xl border bg-slate-100 border-slate-300 p-2 flex items-center justify-center">
+                    <li className="border-r border-slate-300 px-2">Anywhere</li>
+                    <li className="border-r border-slate-300 px-2">Any week</li>
+                    <li className="flex items-center gap-2 px-2">Add guests <span className="h-8 w-8 bg-red-500 text-white rounded-full flex items-center justify-center"><FaSearch></FaSearch></span></li>
+                </ul>
+            </div>
+            <div className="navbar-end">
+            <div className="dropdown dropdown-end">
+                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                    <div className="w-10 rounded-full">
+                        <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                    </div>
+                </label>
+                <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+                    <li>
+                        <a className="justify-between">
+                            Profile
+                            <span className="badge">New</span>
+                        </a>
+                    </li>
+                    <li><a>Settings</a></li>
+                    <li><a>Logout</a></li>
+                </ul>
+            </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
