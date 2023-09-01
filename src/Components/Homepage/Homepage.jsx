ba31ae0fba43b91af61f90da2abef57aa61608a7
Source-Code-007 <utsho926@gmail.com>
2023-09-01 21:56:59 +0600

    test

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -1,7 +1,22 @@
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
+
+
 const Homepage = () => {
     return (
         <div>
-            This is Homepage
+            <Tabs>
+                <TabList>
+                    <Tab>Title 1</Tab>
+                    <Tab>Title 2</Tab>
+                </TabList>
+
+                <TabPanel>
+                    <h2>Any content 1</h2>
+                </TabPanel>
+                <TabPanel>
+                    <h2>Any content 2</h2>
+                </TabPanel>
+            </Tabs>
         </div>
     );
 };
