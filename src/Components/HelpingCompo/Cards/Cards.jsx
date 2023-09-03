1d6b9c3d723de8fca03b567ef20291b01f2080da
Source-Code-007 <utsho926@gmail.com>
2023-09-03 19:21:06 +0600

    loading spinner added

--- src/Components/HelpingCompo/Cards/Cards.jsx
+++ src/Components/HelpingCompo/Cards/Cards.jsx
@@ -5,16 +5,21 @@ const Cards = ({ myData }) => {
     const { title, countryName, facilities, images, property, ratings, rent, reviews } = myData
     console.log(myData);
     return (
-        <div className=' p-3 border-2 border-green-500 rounded-lg'>
-            <div className='flex gap-3 justify-between'>
-                <div className='space-y-2 overflow-hidden'>
-                    <h2 className='font-semibold text-ellipsis whitespace-nowrap overflow-hidden'>{title}</h2>
+        <div className='rounded-lg shadow-lg shadow-gray-400'>
+            <figure>
+                <img src={images[0]} alt="" className='rounded-t-lg h-64 w-full' />
+            </figure>
+            <div className='p-3 space-y-3'>
+                <div className='flex gap-3 justify-between'>
+                    <div className='space-y-2 overflow-hidden'>
+                        <h2 className='font-semibold text-ellipsis whitespace-nowrap overflow-hidden'>{title}</h2>
+                    </div>
+                    <span className='text-slate-900 flex items-center gap-2'><FaStar></FaStar>{ratings}</span>
                 </div>
-                <span className='text-slate-900 flex items-center gap-2'><FaStar></FaStar>{ratings}</span>
-            </div>
-            <p className='font-semibold'>{countryName}</p>
+                <p className='font-semibold'>{countryName}</p>
 
-            <p className='font-bold text-slate-900'>${rent} <span className='text-slate-700 font-semibold'>night</span></p>
+                <p className='font-bold text-slate-900'>${rent} <span className='text-slate-700 font-semibold'>night</span></p>
+            </div>
         </div>
     );
 };
--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -2,12 +2,13 @@ import { useQuery } from "react-query";
 import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 import Cards from "../HelpingCompo/Cards/Cards";
 import { useState } from "react";
+import MyLoading from "../HelpingCompo/MyLoading";
 
 
 
 
 const Homepage = () => {
-    const [activeCategory, setActiveCategory] = useState('countryside')
+    const [activeCategory, setActiveCategory] = useState('Historical homes')
 
     const { isLoading: categoryLoading, data: categories } = useQuery('categories', async () => {
 
@@ -27,7 +28,9 @@ const Homepage = () => {
 
 
     if (categoryLoading || allDataLoading) {
-        return <h2>Loading...</h2>
+        return <div className="h-[96vh] flex items-center justify-center">
+            <MyLoading></MyLoading>
+        </div>
     }
 
 
@@ -37,7 +40,7 @@ const Homepage = () => {
                 <TabList className="flex gap-2 justify-center items-center flex-wrap mb-8">
                     {
                         categories?.map((category, ind) => {
-                            return <Tab key={ind} className={`px-4 py-2 bg-green-500 font-bold text-slate-50 text-xl cursor-pointer rounded`} onClick={() => setActiveCategory(category)}>{category}</Tab>
+                            return <Tab key={ind} className={`px-4 py-2 ${activeCategory === category ? 'bg-green-500' : 'bg-slate-700'} font-bold text-slate-50 text-xl cursor-pointer rounded`} onClick={() => setActiveCategory(category)}>{category}</Tab>
                         })
                     }
                 </TabList>
@@ -46,7 +49,7 @@ const Homepage = () => {
                 {
                     allData.map((data, ind) => {
                         return <TabPanel key={ind}>
-                            <div className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-6 gap-4">
+                            <div className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-5 gap-4">
                                 {
                                      data?.data.map((myData, ind)=> <Cards key={ind} myData={myData}></Cards>)
                                 }
