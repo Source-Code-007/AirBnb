d6866f481223548a0a49e11e9eaf3a5a1398ec3a
Source-Code-007 <utsho926@gmail.com>
2023-09-04 13:08:30 +0600

    modal update

--- src/Components/HelpingCompo/MyModal/MyModal.jsx
+++ src/Components/HelpingCompo/MyModal/MyModal.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaXmark } from "react-icons/fa6";
 
 const MyModal = () => {
+    const [filterRoomsAndBeds, setFilterRoomsAndBeds] = useState({bedrooms:'Any', beds:'Any', bathrooms:'Any' })
+
     return (
         <dialog id="my_modal_1" className="modal">
             <form method="dialog" className="modal-box !p-0 max-w-[800px] relative">
@@ -22,15 +24,47 @@ const MyModal = () => {
                         <h3 className="font-bold text-lg">Price range</h3>
                         <p>Nightly prices before fees and taxes</p>
                     </div>
-                </div>
 
-                {/* <div>
+                    {/* <div>
                     <div className='relative'>
                     <input type="text" defaultValue={1000} className='border border-slate-400 focus:border-slate-800 pt-10 px-6' />
                         <span className='absolute left-1 top-1'>Minimum</span>
                     </div>
                     <input type="text" defaultValue={1000} />
                 </div> */}
+
+                    <div>
+                        <h3 className="font-bold text-lg mb-4 mt-6">Rooms and beds</h3>
+                        <div className='space-y-5'>
+                            <div>
+                                <p className='font-semibold'>Bedrooms</p>
+                                <div className='flex gap-2 flex-wrap'>
+                                    {['Any', '1', '2', '3', '4', '5', '6', '7', '8+'].map((elem, ind) => {
+                                        return <button key={ind} className={`px-4 py-3 rounded hover:none flex-1 capitalize ${filterRoomsAndBeds.bedrooms === elem ? 'bg-black text-white' : 'bg-gray-100 text-black'}`} type='button' onClick={()=> setFilterRoomsAndBeds({...filterRoomsAndBeds, bedrooms: elem})}>{elem}</button>
+                                    })}
+                                </div>
+                            </div>
+                            <div>
+                                <p className='font-semibold'>Beds</p>
+                                <div className='flex gap-2 flex-wrap'>
+                                    {['Any', '1', '2', '3', '4', '5', '6', '7', '8+'].map((elem, ind) => {
+                                        return <button key={ind} className={`px-4 py-3 rounded flex-1 capitalize hover:none ${filterRoomsAndBeds.beds === elem ? 'bg-black text-white' : 'bg-gray-100 text-black'}`} onClick={()=> setFilterRoomsAndBeds({...filterRoomsAndBeds, beds: elem})} type='button'>{elem}</button>
+                                    })}
+                                </div>
+                            </div>
+                            <div>
+                                <p className='font-semibold'>Bathrooms</p>
+                                <div className='flex gap-2 flex-wrap'>
+                                    {['Any', '1', '2', '3', '4', '5', '6', '7', '8+'].map((elem, ind) => {
+                                        return <button key={ind} className={`px-4 py-3 rounded  flex-1 capitalize ${filterRoomsAndBeds.bathrooms === elem? 'bg-black text-white' : 'bg-gray-100 text-black'}`} type='button' onClick={()=> setFilterRoomsAndBeds({...filterRoomsAndBeds, bathrooms: elem})}>{elem}</button>
+                                    })}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
 
                 <div className="modal-action">
                     {/* if there is a button in form, it will close the modal */}
